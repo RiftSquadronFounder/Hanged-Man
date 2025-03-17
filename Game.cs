0b22b17dc167a6afe6b909ce28ca7bfc57184c3e
Echo <fya17102007@gmail.com>
2025-03-17 14:36:50 +0500

    Updated

---
+++ Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanged_Man
+{
+    internal class Game
+    {
+
+        private List<int> randomlyOpenedLetter_;
+        private string userName_;
+        private string word_;
+        private int faultsAssigned_;
+        private int LettersFound_;
+        private int maxFaults_;
+        private Dictionary<int, string> letters_;
+        private bool isRunning_ = true;
+
+        public Record Process_() {
+            
+            
+            
+            
+            
+            
+            
+            
+            return new Record(userName_, word_, faultsAssigned_, LettersFound_, letters_ );
+        }
+
+        private void GameView() { 
+        
+        
+        
+        
+        
+        }
+
+        private void TrySymbol(int index, string symbol) { }
+
+        private void Fail() { }
+
+        private void Win() { }
+
+        private void AssignName() {
+
+            Console.WriteLine
+                ("Похоже на то, что вы не указали имя перед началом игры\n" +
+                "Не хотели бы вы указать ваше имя для таблицы рекордов?");
+        }
+
+
+    }
+}
---
+++ Record.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanged_Man
+{
+    internal class Record
+    {
+        private string userName_;
+        private string word_;
+        private int faults_;
+        private int pickedRight_;
+        private Dictionary<int, string> letters;
+
+
+        public Record(string name, string word, int faults, int rightOnes, Dictionary<int, string> letters)
+        {
+            this.userName_ = name;
+            this.word_ = word;
+            this.faults_ = faults;
+            this.pickedRight_ = rightOnes;
+            this.letters = letters;
+        }
+
+
+        public string GetName() { return userName_; }
+        public string GetWord() { return word_; }
+        public int GetWrongGuesses() {  return faults_; }
+        public int GetRightGuesses() { return pickedRight_; }
+
+        public Dictionary<int, string> GetLetters() {  return letters; }
+
+    }
+
+}
